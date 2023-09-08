9c364ab1f54bdb737c15e504edf5fbf1c5954c5f
Anderson Taguada <anderson@zuluapp.io>
2023-09-08 11:18:24 -0500

    feat: test x

--- test.js
+++ test.js
@@ -6,3 +6,17 @@ console.log(`The name of his is ${data.name}. He is the inventor of ${data.inven
 console.log(`The name of his is ${data.name}. He is the inventor of ${data.inventor} programming language`);
 
 console.log("test")
+
+if (data !== null) {
+    if (data.name !== null) {
+        if (data.name !== "") {
+            if (data.name !== "test") {
+                console.log(data.name)
+            } else {
+                if (data.name === "jajaj") {
+                    console.log("jajaja")
+                }
+            }
+        }
+    } 
+}
